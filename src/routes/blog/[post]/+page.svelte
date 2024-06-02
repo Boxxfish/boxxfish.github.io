c872d7dda439fbe544d5b7f8c8a0c2ab79bdcdf4
Ben Giacalone <bensgiacalone@gmail.com>
2024-06-02 17:43:59 -0400

    Integrate LaTex

--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -1,70 +1,63 @@
 <!-- This file renders each individual blog post for reading. Be sure to update the svelte:head below -->
 <script>
-export let data
+	export let data;
 
-const {
-	title,
-	excerpt,
-	date,
-	updated,
-	coverImage,
-	coverWidth,
-	coverHeight,
-	categories 
-} = data.meta
-const { PostContent } = data
+	const { title, excerpt, date, updated, coverImage, coverWidth, coverHeight, categories } =
+		data.meta;
+	const { PostContent } = data;
 </script>
-
 
 <svelte:head>
 	<!-- Be sure to add your image files and un-comment the lines below -->
 	<title>{title}</title>
-	<meta data-key="description" name="description" content="{excerpt}">
+	<meta data-key="description" name="description" content={excerpt} />
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={title} />
 	<meta name="twitter:title" content={title} />
 	<meta property="og:description" content={excerpt} />
 	<meta name="twitter:description" content={excerpt} />
-	<!-- <meta property="og:image" content="https://yourdomain.com/image_path" /> -->
+	<meta property="og:image" content={coverImage} />
 	<meta property="og:image:width" content={coverWidth} />
 	<meta property="og:image:height" content={coverHeight} />
-	<!-- <meta name="twitter:image" content="https://yourdomain.com/image_path" /> -->
+	<meta name="twitter:image" content={coverImage} />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/katex.min.css">
 </svelte:head>
-
 
 <article class="post">
 	<!-- You might want to add an alt frontmatter attribute. If not, leaving alt blank here works, too. -->
 	<img
 		class="cover-image"
-		src="{coverImage}"
+		src={coverImage}
 		alt=""
 		style="aspect-ratio: {coverWidth} / {coverHeight};"
 		width={coverWidth}
 		height={coverHeight}
 	/>
 
-	<h1>{ title }</h1>
-	
+	<h1>{title}</h1>
+
 	<div class="meta">
-		<b>Published:</b> {date}
-		<br>
-		<b>Updated:</b> {updated}
+		<b>Published:</b>
+		{date}
+		<br />
+		<b>Updated:</b>
+		{updated}
 	</div>
 
 	<svelte:component this={PostContent} />
 
 	{#if categories}
 		<aside class="post-footer">
-			<h2>Posted in: </h2>
+			<h2>Posted in:</h2>
 			<ul>
 				{#each categories as category}
 					<li>
 						<a href="/blog/category/{category}/">
-							{ category }
+							{category}
 						</a>
 					</li>
 				{/each}
 			</ul>
 		</aside>
 	{/if}
-</article> 
+</article>
